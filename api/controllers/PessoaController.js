f3a1e172b447d1a4b64c8c8c7f7dbdc99328d7b8
kaique <kaiquesilva49@hotmail.com>
2022-08-20 10:57:58 -0300

    finalizando crud de pessoa

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -31,6 +31,32 @@ class PessoaController {
       return res.status(500).json(err.message);
     }
   }
+
+  static async atualizarPessoa(req, res) {
+    const pessoa = req.body;
+    const { id } = req.params;
+    try {
+      await database.Pessoas.update(pessoa, {
+        where: { id: Number(id) },
+      });
+      const pessoaAtualizada = await database.Pessoas.findOne({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json(pessoaAtualizada);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async deletar(req, res) {
+    const { id } = req.params;
+    try {
+      await database.Pessoas.destroy({ where: { id: Number(id) } });
+      return res.status(200);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
 }
 
 module.exports = PessoaController;
