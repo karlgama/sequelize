dcb029795111c8be0c36408c1f60bf326bd034d2
kaique <kaiquesilva49@hotmail.com>
2022-08-23 15:15:32 -0300

    criando migrações de outras entidades

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,6 +4,7 @@ const pessoas = require("./pessoasRoute");
 module.exports = (app) => {
   app.use(express.json());
   app.use(pessoas);
+  
   app.get("/", (req, res) => {
     res.send("teste");
   });
